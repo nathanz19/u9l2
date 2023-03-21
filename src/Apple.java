df030906d2d4b26b65fd3aace5e49724bca07dbb
Nathan Zheng <nathanz@nycstudents.net>
2023-03-21 19:17:24 -0400

    Initial commit

---
+++ src/Apple.java
@@ -0,0 +1,13 @@
+public class Apple {
+    private int numProducts;
+
+    public Apple(int numProducts) {
+        this.numProducts = numProducts;
+    }
+
+    public int getNumProducts() {
+        return numProducts;
+    }
+
+    public void make
+}
